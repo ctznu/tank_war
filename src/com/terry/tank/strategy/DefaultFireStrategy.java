5e0b6466ecd949b9a3afe32d23d2ecb247246988
ctznu <ctzn2010@qq.com>
2020-04-10 00:38:19 +0800

    revert - decorator tail rectangle

--- src/com/terry/tank/strategy/DefaultFireStrategy.java
+++ src/com/terry/tank/strategy/DefaultFireStrategy.java
@@ -9,12 +9,10 @@ public class DefaultFireStrategy implements FireStrategy {
     public void fire(Tank t) {
         int bX = t.x + (Tank.WIDTH - Bullet.WIDTH)/2;
         int bY = t.y + (Tank.HEIGHT - Bullet.HEIGHT)/2;
-        GameModel.getInstance().add(
+        /*GameModel.getInstance().add(
                 new RectDecorator(
-                        new TailDecorator(new Bullet(bX, bY, t.dir, t.group)
-                        )
-                )
-        );
+                        new TailDecorator(new Bullet(bX, bY, t.dir, t.group))));*/
+        new Bullet(bX, bY, t.dir, t.group);
         if (t.group == Group.GOOD) {
 //            new Thread(()->new Audio("audio/tank_fire.wav").play()).start();
         }
